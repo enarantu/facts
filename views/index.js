50959ff834790a7e99eab8c48cb21de5c52796ce
Enkhtur Narantungalag <enarantu@umich.edu>
2018-07-23 19:25:34 -0400

    removed fluff

--- views/index.js
+++ views/index.js
@@ -7,26 +7,6 @@ app.get('/', function(req, res){
   res.sendFile(appDir + '/html/index.html');
 });
 
-var countdown = 5000;
-setInterval(function() {  
-  countdown--;
-  io.sockets.emit('timer', { countdown: countdown });
-}, 1000);
-
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  countdown = 1000;
-  socket.on('chat message', function(msg){
-  	
-  	io.sockets.emit('timer', {countdown: countdown});
-  	io.emit('chat message',msg);
-  	console.log('message: ' + msg);
-  });
-});
-
-io.emit('some event', { for: 'everyone' });
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
