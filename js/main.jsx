b7a82c41b544159c8cc78eb390c3499707fc60e4
Enkhtur Narantungalag <enarantu@umich.edu>
2018-10-17 11:55:47 -0400

    1 decided client prediction should only be for the player not for other players 2 documented some functions 3 fixed package.json

--- js/main.jsx
+++ js/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import io from 'socket.io-client';
+import io from 'socket.io-client'
 
 var KEY = {
     LEFT:  37,
     RIGHT: 39,
     UP: 38,
     DOWN: 40
-};
-
+}
 
 function move(locations, direction){
+    /*
+    REQUIRES: direction should be a valid direction, snake should be a valid snake
+    MODIFIES: locations of the snake
+    EFFECTS: moves the snake
+    */
     function moveU(locations){
         const n = locations.length
         locations.push( 
@@ -29,7 +33,6 @@ function move(locations, direction){
             y : locations[n-1].y + 10
         })
         locations.shift()
-
     }
     function moveR(locations){
         const n = locations.length
@@ -71,90 +74,107 @@ function move(locations, direction){
 
 class Game extends React.Component {
     constructor() {
-        super();
+        super()
         this.state = {
             started: false,
-            name: '',
-            locations: {},
-            directions: {},
-            timesteps: {},
-            
+            player_data: {},
+            others_data: {},
             endpoint: 'localhost'
         }
         this.socket = null
-        this.request_queue = []
-        this.timestep = -1
+        this.newdir = ''
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleSubmit(event){
-        this.socket = io(this.state.endpoint);
-        this.socket.emit("new-player", this.state.name);
-        this.socket.on("new-player", (data) => {
-            console.log('new-player', data)
-            clientstep = data.timestep + 1
-        })
-        setInterval(() => {clientstep += 1} , 250)
-        this.state.started = true
-        this.setState({})
-    }
-    handleChange(event){
-        this.setState({
-            name: event.target.value,
-        });
-    }
-
-    bindKeys() {
-        window.addEventListener('keydown', this.handleKeys.bind(this));
-    }
-
-    unbindKeys() {
-        window.removeEventListener('keydown', this.handleKeys);
-    }
-    handleKeys(e){
-        if( request_queue.length == 0 || request_queue[request_queue.length - 1].step < clientstep ){
-            var msg = {}
-            msg.request_type = 'dir-change'
-            msg.player_name = this.state.name
-            switch (e.keyCode) {
-                case KEY.LEFT:
-                    msg.dir = "L"
-                    break;
-                case KEY.RIGHT:
-                    msg.dir = "R"
-                    break;
-                case KEY.UP:
-                    msg.dir = "U"
-                    break;
-                case KEY.DOWN:
-                    msg.dir = "D"
-                    break;
-                default:
-                    return
-            }
-            if(msg.dir === dir){
-                return
-            }
-            dir = msg.dir
-            msg.step = clientstep
-            if(this.state.started){
-                this.socket.emit('request', msg)
-                request_queue.push(msg)
-            }
-        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleKeys   = this.handleKeys.bind(this)
     }
     componentDidMount() {
-        this.bindKeys(); 
-        this.drawPlayers();
+        this.bindKeys()
+        this.drawPlayers()
     }
     componentDidUpdate() {
         this.drawPlayers()
     }
     componentWillUnmount() {
-        this.unbindKeys();
+        this.unbindKeys()
+    }
+    handleSubmit(event){
+        /*
+        REQUIRES: user to click submit
+        MODIFIES: this.state.started
+        EFFECTS:  declares the game to started and starts game
+        */
+        this.socket = io(this.state.endpoint);
+        this.state.started = true
+        game()
+    }
+    game(){
+        /*
+        REQUIRES: this.state.started to be true
+        MODIFIES: everything
+        EFFECTS: controls the game
+        */
+        this.socket.emit("new-player", this.state.name);
+        setInterval(() => {
+            this.setState({})
+        } , 250)
+        
+    }
+
+    handleChange(event){
+        /*
+        REQUIRES: keyboard to be bound
+        MODIFIES: player name
+        EFFECTS : setState is called, so rerenders
+        */
+        this.setState({
+            player_data : {name : event.target.value}
+        });
+    }
+
+    bindKeys() {
+        window.addEventListener('keydown', this.handleKeys);
+    }
+    unbindKeys() {
+        window.removeEventListener('keydown', this.handleKeys);
+    }
+    handleKeys(e){
+        /*
+        REQUIRES: player has given a name and connected to the game
+        MODIFIES: new direction variable this.newdir
+        EFFECTS:  prepares this.newdir for the next frame
+        */
+        switch (e.keyCode) {
+            case KEY.LEFT:
+                if(this.player_data.dir !== "R"){
+                    this.newdir = "L"
+                }
+                break
+            case KEY.RIGHT:
+                if(this.player_data.dir !== "L"){
+                    this.newdir = "R"
+                }
+                break
+            case KEY.UP:
+                if(this.player_data.dir !== "D"){
+                    this.newdir = "U"
+                }
+                break;
+            case KEY.DOWN:
+                if(this.player_data.dir !== "U"){
+                    this.newdir = "D"
+                }
+                break
+            default:
+                return
+        }
     }
     drawPlayers() {
+        /*
+        REQUIRES: component needs to be mounted
+        MODIFIES: canvas
+        EFFECTS : draws snakes on canvas
+        */
         const ctx = this.refs.canvas.getContext('2d');
         ctx.clearRect(0, 0, 1200, 800);
         Object.keys(this.state.players).forEach( player => {
@@ -166,16 +186,14 @@ class Game extends React.Component {
     render() {
         return (
             <div>
-                <canvas ref="canvas" width={800} height={600} style={{border:"1px solid #000000", float: "left"}}> </canvas>
-                <div ref="names" width={200}>
-                </div>
-                {!this.state.started &&
-                 <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        onChange={this.handleChange}
-                     />
-                </form>
+                <canvas ref="canvas" width={800} height={600} 
+                        style={{border:"1px solid #000000", float: "left"}}> </canvas>
+                <div ref="names" width={200}></div>
+                {
+                    !this.state.started &&
+                     <form onSubmit={this.handleSubmit}>
+                        <input type="text" onChange={this.handleChange}/>
+                    </form>
                 }
                 {
                     this.state.started &&
@@ -183,11 +201,12 @@ class Game extends React.Component {
                 }
                 <div style={{whiteSpace:"pre"}}>
                     State: <br></br>
-                    {JSON.stringify(this.state, undefined, 4)}
+                    {
+                        JSON.stringify(this.state, undefined, 2)
+                    }
                 </div>
             </div>
         );
     }
 }
-
-ReactDOM.render(<Game/>, document.getElementById('app'));
+ReactDOM.render(<Game/>, document.getElementById('app'))
